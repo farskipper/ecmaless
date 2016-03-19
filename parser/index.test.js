288182ead350d8a460d307f4627094ce2986f7e5
Matthew Wright <matthew@smallhelm.com>
2016-03-19 12:05:02 -0600

    easy way of testing the parser

--- parser/index.test.js
+++ parser/index.test.js
@@ -2,19 +2,29 @@ var _ = require("lodash");
 var test = require("tape");
 var parser = require("./");
 
-var astWoLocAndSrc = function(ast){
+var printAst = function(ast){
   if(_.isArray(ast)){
-    return _.map(ast, astWoLocAndSrc);
+    return _.map(ast, printAst).join(" ");
+  }else if(ast.type === 'list'){
+    return "(" + printAst(ast.value) + ")";
+  }else if(ast.type === 'number'){
+    return ast.value;
+  }else if(ast.type === 'string'){
+    return JSON.stringify(ast.value);
+  }else if(ast.type === 'symbol'){
+    return ast.value;
   }
-  return {
-    type: ast.type,
-    value: ast.type === 'list' ? astWoLocAndSrc(ast.value) : ast.value
-  };
+  throw new Error("Unknown ast_node type: " + ast.type);
 };
 
-test("parse numbers", function(t){
-  console.log(parser("(1 2)"));
+var testParse = function(t, src, expected, msg){
+  t.equals(printAst(parser(src)), expected, msg || "didn't parse as expected");
+};
+
+test("parser", function(t){
+  var tp = _.partial(testParse, t);
+  tp(" 1\n2", "1 2");
+  tp(" (+ 1 2\n(  /\n1 4)) ", "(+ 1 2 (/ 1 4))");
+  //tp("<1 2 3>", "(<> 1 2 3)");
   t.end();
-//console.log(parser("[\"one\"2<three :four>]"));
-//console.log(JSON.stringify(astWoLocAndSrc(parser("(\"one\"2)")), undefined, 2));
 });
