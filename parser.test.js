58e0a3d597b94c237e981f4fbe23b17486620bd6
Matthew Wright <matthew@smallhelm.com>
2016-03-19 09:52:24 -0600

    parser build ast nodes

--- parser.test.js
+++ parser.test.js
@@ -1,3 +1,15 @@
+var _ = require("lodash");
 var parser = require("./parser");
 
-console.log(parser("[\"one\"2<three :four>]"));
+var astWoLocAndSrc = function(ast){
+  if(_.isArray(ast)){
+    return _.map(ast, astWoLocAndSrc);
+  }
+  return {
+    type: ast.type,
+    value: ast.type === 'list' ? astWoLocAndSrc(ast.value) : ast.value
+  };
+};
+
+//console.log(parser("[\"one\"2<three :four>]"));
+console.log(JSON.stringify(astWoLocAndSrc(parser("(\"one\"2)")), undefined, 2));
