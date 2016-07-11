3dc415b15cd39305e08262ce0de5236c96ea5196
Matthew Wright <matthew@smallhelm.com>
2016-07-11 16:19:59 -0600

    fixing tokenizing numbers

--- packages/ecmaless-parser/tokenizer.js
+++ packages/ecmaless-parser/tokenizer.js
@@ -6,19 +6,29 @@ module.exports = function(src){
   var tokens = [];
   var index = 0;
   var t = tokenizer2(function(tok){
-    tokens.push({
-      type: tok.type,
-      src: tok.src,
-      loc: toLoc(index, index + tok.src.length)
-    });
+    var prev = tokens[tokens.length - 1];
+    if(prev && prev.type === tok.type){
+      prev.src += tok.src;
+      prev.loc.source = prev.src;
+      prev.loc.end = toLoc(index, index + tok.src.length).end;
+    }else{
+      tokens.push({
+        type: tok.type,
+        src: tok.src,
+        loc: toLoc(index, index + tok.src.length)
+      });
+    }
     index += tok.src.length;
   });
 
   t.addRule(/^ +$/, "Space");
-  t.addRule(/^\n+$/, "NewLine");
+  t.addRule(/^\n$/, "NewLine");
   t.addRule(/^;[^\n]*$/, "Comment");
   t.addRule(/(^""$)|(^"([^"]|\\")*[^\\]"$)/, "String");
-  t.addRule(/^([0-9]+)|([0-9]+\.[0-9]+)|(\.[0-9]+)$/, "Number");
+  t.addRule(/^[0-9]+\.[0-9]+$/, "Number");
+  t.addRule(/^\.[0-9]+$/, "Number");
+  t.addRule(/^[0-9]+$/, "Number");
+  t.addRule(/^[a-zA-Z_][a-zA-Z0-9_]*$/, "Symbol");
 
   t.onText(src);
   t.end();
--- packages/ecmaless-parser/tokenizer.test.js
+++ packages/ecmaless-parser/tokenizer.test.js
@@ -1,3 +1,4 @@
+var _ = require("lodash");
 var test = require("tape");
 var tokenizer = require("./tokenizer");
 
@@ -10,6 +11,20 @@ test("tokenizer", function(t){
       loc: {source: '123', start: {line: 1, column: 0}, end: {line: 1, column: 3}}
     }
   ]);
+  t.deepEquals(tokenizer("123.45"), [
+    {
+      type: 'Number',
+      src: '123.45',
+      loc: {source: '123.45', start: {line: 1, column: 0}, end: {line: 1, column: 6}}
+    }
+  ]);
+
+  var testOrder = function(src, tok_order){
+    t.deepEquals(_.map(tokenizer(src), "type"), tok_order);
+  };
+
+  testOrder('123 "four"\nblah', ["Number", "Space", "String", "NewLine", "Symbol"]);
+  testOrder('10 .1 0.1 1.0', ["Number", "Space", "Number", "Space", "Number", "Space", "Number"]);
 
   t.end();
 });
