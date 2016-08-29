e0931c776779c56bcf7708e09c429466e05d313c
Matthew Wright <matthew@smallhelm.com>
2016-08-29 14:31:00 -0500

    cleanup

--- packages/ecmaless-stdlib/loader.js
+++ packages/ecmaless-stdlib/loader.js
@@ -5,7 +5,7 @@ var core_path = path.resolve(__dirname, "./src/core.js");
 
 
 module.exports = function(sym){
-  if(core.hasOwnProperty(sym))){
+  if(core.has(core, sym)){
     return core_path;
   }
   //TODO
