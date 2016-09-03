e65bcf08ddb2ddb8beb9d6935d70d2c2fc60a35b
Matthew Wright <matthew@smallhelm.com>
2016-09-02 22:50:55 -0500

    map

--- packages/ecmaless-stdlib/loader.js
+++ packages/ecmaless-stdlib/loader.js
@@ -1,12 +1,23 @@
+var fs = require("fs");
 var path = require("path");
 var core = require("./src/core");
 
-var core_path = path.resolve(__dirname, "./src/core.js");
+var lookup = {};
 
+var src_dir = path.resolve(__dirname, "src/");
+fs.readdirSync(src_dir).forEach(function(filename){
+  var file = path.resolve(src_dir, filename);
+
+  if(filename === "core.js"){
+    Object.keys(core).forEach(function(sym){
+      lookup[sym] = {file: file, key: sym};
+    });
+    return;
+  }
+  var sym = filename.split(".")[0];
+  lookup[sym] = {file: file};
+});
 
 module.exports = function(sym){
-  if(core.has(core, sym)){
-    return core_path;
-  }
-  //TODO
+  return lookup[sym];
 };
