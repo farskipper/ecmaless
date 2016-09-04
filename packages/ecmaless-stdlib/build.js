a91db2c63d5b8e386d26b0d4340651acd2048da9
Matthew Wright <matthew@smallhelm.com>
2016-09-03 22:52:28 -0500

    starting self hosted index

---
+++ packages/ecmaless-stdlib/build.js
@@ -0,0 +1,36 @@
+var fs = require("fs");
+var path = require("path");
+var core = require("./src/core");
+
+var lookup = {};
+
+fs.readdirSync(path.resolve(__dirname, "src/")).forEach(function(filename){
+  var file = path.join("src", filename);
+
+  if(filename === "core.js"){
+    Object.keys(core).forEach(function(sym){
+      lookup[sym] = {file: file, key: sym};
+    });
+    return;
+  }
+  var sym = filename.split(".")[0];
+  lookup[sym] = {file: file};
+});
+
+var index = "";
+var index_part2 = "";
+index += "deps:\n";
+index += "    core \"./src/index.js\"\n";
+core.iterate(lookup, function(o, sym){
+  if(!/core\.js$/.test(o.file)){
+    index += "    " + sym + " " + JSON.stringify("./" + o.file) + "\n";
+    index_part2 += "stdlib." + sym + " = " + sym + "\n";
+  }
+  return true;
+});
+index += "\ndef stdlib = core\n";
+index += index_part2;
+index += "stdlib";
+
+fs.writeFileSync("./lookup.json", JSON.stringify(lookup, void 0, 2));
+fs.writeFileSync("./index.ecmaless", index);
--- packages/ecmaless-stdlib/loader.js
+++ packages/ecmaless-stdlib/loader.js
@@ -1,21 +1,8 @@
-var fs = require("fs");
 var path = require("path");
-var core = require("./src/core");
+var lookup = require("./lookup.json");
 
-var lookup = {};
-
-var src_dir = path.resolve(__dirname, "src/");
-fs.readdirSync(src_dir).forEach(function(filename){
-  var file = path.resolve(src_dir, filename);
-
-  if(filename === "core.js"){
-    Object.keys(core).forEach(function(sym){
-      lookup[sym] = {file: file, key: sym};
-    });
-    return;
-  }
-  var sym = filename.split(".")[0];
-  lookup[sym] = {file: file};
+Object.keys(lookup).forEach(function(sym){
+  lookup[sym].file = path.resolve(__dirname, lookup[sym].file);
 });
 
 module.exports = function(sym){
