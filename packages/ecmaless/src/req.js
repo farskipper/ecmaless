b28ee5b1880e00cd0d03521238cb5d649a9f11f5
Matthew Wright <matthew@smallhelm.com>
2017-10-06 15:49:40 -0600

    restructure main ecmaless package

---
+++ packages/ecmaless/src/req.js
@@ -0,0 +1,20 @@
+function require(mdefs, main){//eslint-disable-line
+    var loadModule = function loadModule(mid) {
+        var m = mdefs[mid];
+        var args = [];
+        var i = 1;
+        while (i < m.length) {
+            args.push(require(m[i]));
+            i++;
+        }
+        return m[0].apply(void 0, args);
+    };
+    var cache = {};
+    var require = function(mid){
+        if(!cache.hasOwnProperty(mid)){
+            cache[mid] = loadModule(mid);
+        }
+        return cache[mid];
+    };
+    return require(main);
+}
